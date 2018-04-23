c8c40db1915dfc8d5f1dc3989b2dc57e103f4886
Dariusz Bożek <dariusz.bozek@mbdvc.pl>
2018-04-23 13:22:51 +0200

    Zadanie 1

--- HRA/HRA/App.xaml.cs
+++ HRA/HRA/App.xaml.cs
@@ -8,10 +8,15 @@ using System.Windows;
 
 namespace HRA
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
+ 
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var bs = new Bootstrapper();
+            bs.Run();
+        }
     }
 }
---
+++ HRA/HRA/Bootstrapper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Prism.Unity;
+using Microsoft.Practices.Unity;
+
+using System.Windows;
+
+namespace HRA
+{
+    class Bootstrapper : UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void InitializeShell()
+        {
+            Application.Current.MainWindow.Show();
+        }
+    }
+}
