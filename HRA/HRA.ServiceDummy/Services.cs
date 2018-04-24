3e5039146e1c0bb48925886b58ff1eff165dd84b
Dariusz Bożek <dariusz.bozek@mbdvc.pl>
2018-04-24 10:14:41 +0200

    Zadanie 3

---
+++ HRA/HRA.ServiceDummy/Services.cs
@@ -0,0 +1,15 @@
+﻿using HRA.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace HRA.ServiceDummy
+{
+    public class Services: IService
+    {
+
+    }
+}
